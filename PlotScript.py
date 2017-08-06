bb116c2e71ccfebce639d17089e57c6842856979
aniliitb10 <aniliitb10@gmail.com>
2017-08-07 01:19:27 +0530

    Took lables out of plot()

--- PlotScript.py
+++ PlotScript.py
@@ -13,7 +13,7 @@ line_styles = ['-', '--', '-.', ':']  # ref:http://matplotlib.org/api/lines_api.
 counter = [0]
 
 
-def plot(x, y, label="", x_label="x-axis", y_label="y-axis"):
+def plot(x, y, label=""):
 
     counter[0] += 1
     plt.plot(x, y)
@@ -33,9 +33,6 @@ def plot(x, y, label="", x_label="x-axis", y_label="y-axis"):
              markersize=8,
              label=label)  # for the legend
 
-    plt.xlabel(x_label, fontsize=font_size)
-    plt.ylabel(y_label, fontsize=font_size)
-
 
 def main():
     x_axis = np.linspace(0, 2*np.pi, 100)  # start, stop, number of divisions
@@ -44,12 +41,15 @@ def main():
     plot(x_axis, y_axis_sin, "sin(x)")
     plot(x_axis, y_axis_cos, "cos(x)")
 
-    plt.title("sin(x)/cos(x) Plot", fontsize=font_size)
+    plt.title("sin(x) vs cos(x)", fontsize=font_size)
 
     # plt.xticks: if an array of strings is provided as 2nd argument, it links with corresponding entry of 1st array
     # x_ticks_array = ["0", "pi/2", "pi", "1.5*pi", "2* pi"]
     # plt.xticks(np.linspace(0, 2*np.pi, len(x_ticks_array)), x_ticks_array),
     plt.xticks(np.linspace(0, 2*np.pi, numOfTicksOnXAxis))
+
+    plt.xlabel("x-values (in radian)", fontsize=font_size)
+    plt.ylabel("sin(x)/cos(x)", fontsize=font_size)
 
     plt.grid()
     plt.legend()
